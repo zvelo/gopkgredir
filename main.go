9e910ccc7add584f79e5992283bc67993f615566
Joshua Rubin <jrubin@zvelo.com>
2016-03-14 16:59:40 -0600

    add support for tls

--- main.go
+++ main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 	"os"
 	"path"
+	"sync"
 	"time"
 
-	log "github.com/Sirupsen/logrus"
 	"github.com/codegangsta/cli"
-	"github.com/gin-gonic/contrib/ginrus"
-	"github.com/gin-gonic/contrib/gzip"
 	"github.com/gin-gonic/gin"
 	"github.com/tylerb/graceful"
 )
@@ -35,11 +34,14 @@ const (
 )
 
 type config struct {
-	ImportPrefix  string
-	VCS           string
-	RepoRoot      string
-	RedirectURL   string
-	ListenAddress string
+	ImportPrefix     string
+	VCS              string
+	RepoRoot         string
+	RedirectURL      string
+	ListenAddress    string
+	TLSListenAddress string
+	TLSCertFile      string
+	TLSKeyFile       string
 }
 
 type context struct {
@@ -51,7 +53,7 @@ var (
 	html   *template.Template
 	cfg    config
 	app    = cli.NewApp()
-	engine = gin.New()
+	engine = gin.Default()
 )
 
 func init() {
@@ -72,63 +74,106 @@ func init() {
 			Usage:  "base url used for the vanity url, any part of the path after that given here is considered <package>",
 		},
 		cli.StringFlag{
-			Name:   "vcs, c",
+			Name:   "vcs",
 			EnvVar: "VCS",
 			Value:  "git",
 			Usage:  "vcs repo type",
 		},
 		cli.StringFlag{
-			Name:   "repo-root, u",
+			Name:   "repo-root",
 			EnvVar: "REPO_ROOT",
 			Usage:  "base url used for the repo package path, /<package> is appended ",
 		},
 		cli.StringFlag{
-			Name:   "redirect-url, r",
+			Name:   "redirect-url",
 			EnvVar: "REDIRECT_URL",
 			Usage:  "url to redirect browsers to, if empty, redirects to repo-root/package",
 		},
 		cli.StringFlag{
-			Name:   "listen-address, l",
+			Name:   "listen-address",
 			EnvVar: "LISTEN_ADDRESS",
 			Value:  "[::]:80",
 			Usage:  "address (ip/hostname and port) that the server should listen on",
 		},
+		cli.StringFlag{
+			Name:   "tls-listen-address",
+			EnvVar: "TLS_LISTEN_ADDRESS",
+			Value:  "[::]:443",
+			Usage:  "address (ip/hostname and port) that the server should listen on for tls requests",
+		},
+		cli.StringFlag{
+			Name:   "tls-cert-file",
+			EnvVar: "TLS_CERT_FILE",
+			Usage:  "tls certificate bundle",
+		},
+		cli.StringFlag{
+			Name:   "tls-key-file",
+			EnvVar: "TLS_KEY_FILE",
+			Usage:  "tls key file",
+		},
 	}
+
+	engine.SetHTMLTemplate(html)
+	engine.GET("/*package", handler)
 }
 
 func main() {
 	if err := app.Run(os.Args); err != nil {
-		log.WithField("error", err).Fatal("app returned error")
+		log.Fatalln("app returned error", err)
 	}
 }
 
 func setup(c *cli.Context) error {
-	gin.SetMode(gin.ReleaseMode)
-
-	engine.Use(
-		gin.Recovery(),
-		ginrus.Ginrus(log.StandardLogger(), time.RFC3339, true),
-		gzip.Gzip(gzip.DefaultCompression),
-	)
-
-	engine.SetHTMLTemplate(html)
-
-	engine.GET("/*package", handler)
-
 	cfg = config{
-		ImportPrefix:  c.String("import-prefix"),
-		VCS:           c.String("vcs"),
-		RepoRoot:      c.String("repo-root"),
-		RedirectURL:   c.String("redirect-url"),
-		ListenAddress: c.String("listen-address"),
+		ImportPrefix:     c.String("import-prefix"),
+		VCS:              c.String("vcs"),
+		RepoRoot:         c.String("repo-root"),
+		RedirectURL:      c.String("redirect-url"),
+		ListenAddress:    c.String("listen-address"),
+		TLSListenAddress: c.String("tls-listen-address"),
+		TLSCertFile:      c.String("tls-cert-file"),
+		TLSKeyFile:       c.String("tls-key-file"),
 	}
 
 	return nil
 }
 
 func run(c *cli.Context) {
-	log.Infof("listening at %s", cfg.ListenAddress)
-	graceful.Run(cfg.ListenAddress, 10*time.Second, engine)
+	httpServer := &graceful.Server{
+		Timeout: 10 * time.Second,
+		Server:  &http.Server{Addr: cfg.ListenAddress, Handler: engine},
+	}
+
+	tlsServer := &graceful.Server{
+		Timeout: 10 * time.Second,
+		Server:  &http.Server{Addr: cfg.TLSListenAddress, Handler: engine},
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		// This always returns an error on exit
+		log.Printf("listening for http at %s", cfg.ListenAddress)
+		if err := httpServer.ListenAndServe(); err != nil {
+			log.Fatalln("http server failure", err)
+		}
+	}()
+
+	if len(cfg.TLSCertFile) > 0 && len(cfg.TLSKeyFile) > 0 {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			log.Printf("listening for tls at %s (%s, %s)", cfg.TLSListenAddress, cfg.TLSCertFile, cfg.TLSKeyFile)
+			if err := tlsServer.ListenAndServeTLS(cfg.TLSCertFile, cfg.TLSKeyFile); err != nil {
+				log.Fatalln("tls server failure", err)
+			}
+		}()
+	}
+
+	// wait for the graceful servers to stop
+	wg.Wait()
 }
 
 func handler(c *gin.Context) {
